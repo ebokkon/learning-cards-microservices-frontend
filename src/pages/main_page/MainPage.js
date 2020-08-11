47e55e04044f54950b5c9b637a56688bfb7021f1
Kornel Leitner <leitner.kornel@gmail.com>
2020-08-11 16:10:17 +0200

    Main page statements added

--- src/pages/main_page/MainPage.js
+++ src/pages/main_page/MainPage.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import "./MainPage.css"
+import Login from "../authentication_pages/Login";
 
 const MainPage = () => {
-  return (
-      <div>
-        <h2>Main page</h2>
-      </div>
-  );
+    //localStorage.setItem("username", "User")
+    localStorage.clear();
+    console.log(localStorage.getItem("username"));
+
+    if (localStorage.getItem("username")) {
+        return (
+            <div>
+                <h2>Main page</h2>
+                <p>{`Welcome ${localStorage.getItem("username")}`}</p>
+            </div>
+        );
+    } else {
+        return (
+        <Login/>
+        );
+    }
+
 };
 
 export default MainPage;
