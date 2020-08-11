d1bf3aabf6c2b81ad6efade89b4ad2ec7f3c1e3d
Kornel Leitner <leitner.kornel@gmail.com>
2020-08-11 16:07:26 +0200

    Create basic very implementation of registration page

--- src/pages/authentication_pages/Registration.js
+++ src/pages/authentication_pages/Registration.js
@@ -5,6 +5,16 @@ const Registration = () => {
   return (
       <div>
         <h2>Registration</h2>
+          <div>
+              <form className="registration-form">
+                  <label htmlFor="registration-username-input">Username</label>
+                  <input type="text" name="registration-username-input" className="registration-username"/>
+                  <label htmlFor="registration-password-input">Password</label>
+                  <input type="password" name="registration-password-input" className="registration-password"/>
+                  <input type="submit" className="registration-button" value="Register"/>
+                  {/*Need email adress field too,*/}
+              </form>
+          </div>
       </div>
   );
 };
