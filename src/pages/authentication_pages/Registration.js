cb78be953f6cbf79bf9f8761266d6bb0c5cf8409
Kornel Leitner <leitner.kornel@gmail.com>
2020-08-12 10:56:13 +0200

    Basic implementation of registration added

--- src/pages/authentication_pages/Registration.js
+++ src/pages/authentication_pages/Registration.js
@@ -2,21 +2,31 @@ import React from 'react';
 import "./Authentication.css"
 
 const Registration = () => {
-  return (
-      <div>
-        <h2>Registration</h2>
-          <div>
-              <form className="registration-form">
-                  <label htmlFor="registration-username-input">Username</label>
-                  <input type="text" name="registration-username-input" className="registration-username"/>
-                  <label htmlFor="registration-password-input">Password</label>
-                  <input type="password" name="registration-password-input" className="registration-password"/>
-                  <input type="submit" className="registration-button" value="Register"/>
-                  {/*Need email adress field too,*/}
-              </form>
-          </div>
-      </div>
-  );
+    const sendRegistrationDetails = (event) => {
+        event.preventDefault();
+        alert("Registration button pressed!")
+    }
+
+    return (
+        <div className="registration-page">
+            <div className="registration-container">
+                <div className="registration-text-container">
+                    <h2 className="registration-title">Registration</h2>
+                </div>
+                <div className="registration-form-container">
+                    <form className="registration-form" onSubmit={sendRegistrationDetails}>
+                        <label htmlFor="registration-username-input" className="registration-label">Username</label>
+                        <input type="text" name="registration-username-input" placeholder="Type your username" className="registration-input" autoComplete="off" required/>
+                        <label htmlFor="registration-password-input" className="registration-label">Password</label>
+                        <input type="password" name="registration-password-input" placeholder="Type your password" className="registration-input" autoComplete="off" required/>
+                        <label htmlFor="registration-email-input" className="registration-label">Email</label>
+                        <input type="email" name="registration-email-input" placeholder="Type your email address" className="registration-input" autoComplete="off" required/>
+                        <input type="submit" className="registration-button" value="Register"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Registration;
