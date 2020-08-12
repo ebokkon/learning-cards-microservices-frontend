14f3511d91115b20c6a78aa2f50977e546dbf6ce
Kornel Leitner <leitner.kornel@gmail.com>
2020-08-12 14:52:45 +0200

    Deck created in card structure

--- src/pages/main_page/MainPage.js
+++ src/pages/main_page/MainPage.js
@@ -1,22 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./MainPage.css"
 import Login from "../authentication_pages/Login";
+import {Link} from "react-router-dom";
 
 const MainPage = () => {
-    //localStorage.setItem("username", "User")
-    localStorage.clear();
+    localStorage.setItem("username", "test user")
+    //localStorage.clear();
     console.log(localStorage.getItem("username"));
+
+    const [decks, setDecks] = useState([
+        {"id": 1, "name": "animals", "description": "Test description, it is a longer text."},
+        {"id": 2, "name": "capitals", "description": "Test description, it is a longer text."},
+        {"id": 3, "name": "countries", "description": "Test description, it is a longer text."},
+        {"id": 4, "name": "cars", "description": "Test description, it is a longer text."}
+    ])
+    console.log(decks);
 
     if (localStorage.getItem("username")) {
         return (
             <div>
                 <h2>Main page</h2>
                 <p>{`Welcome ${localStorage.getItem("username")}`}</p>
+                <section className="deck-cards">
+                    {decks.map((deck) => {
+                        return (
+                            <div className="deck-card-element">
+                                <div className="deck-title">
+                                    <p className="deck-text">{deck.name}</p>
+                                </div>
+                                <div className="deck-description">
+                                    <p className="deck-text">{deck.description}</p>
+                                </div>
+                                <div className="deck-image-container">
+                                    <img src={"/deck.png"} className="deck-image" alt="Deck"/>
+                                </div>
+                                <div className="deck-play-button">
+                                    <Link to="/game" className="play-link">
+                                        <div className="play-button">Study</div>
+                                    </Link>
+                                </div>
+                                <div className="deck-button-container">
+                                    <Link to={`/deck/${deck["id"]}`} className="decks-cards-button-link">
+                                        <div className="deck-button-text">Cards</div>
+                                    </Link>
+                                    <div className="delete-deck-button">
+                                        <div className="deck-button-text">
+                                            Delete
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                            ;
+                    })}
+                </section>
             </div>
         );
     } else {
         return (
-        <Login/>
+            <Login/>
         );
     }
 
