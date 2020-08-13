a342db363a0e2a194607f7d20c3eaf0fd0a53786
Kornél <leitner.kornel@gmail.com>
2020-08-13 18:35:59 +0200

    Fix delete function

--- src/pages/main_page/MainPage.js
+++ src/pages/main_page/MainPage.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./MainPage.css";
 import Login from "../authentication_pages/Login";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import axios from "axios";
 
 const MainPage = () => {
   localStorage.setItem("username", "test user");
-  localStorage.setItem("id", 1);
   //localStorage.clear();
   console.log(localStorage.getItem("username"));
 
@@ -18,59 +17,63 @@ const MainPage = () => {
     });
   }, []);
 
-  function DeleteDeck() {
-    //TODO connect with id
+  const deleteDeck = (event) => {
+    event.preventDefault();
+    let currentId = event.target.dataset.id;
+    event.persist();
     axios
-      .delete(`http://localhost:8762/deck-handler/deck/${id}`)
-      .then((res) => {
-        console.log(res.data);
-      });
+        .delete(`http://localhost:8762/deck-handler/deck/${currentId}`)
+        .then((res) => {
+          if (event.target.classList.contains("deck-button-text")) {
+            event.target.parentElement.parentElement.parentElement.style.display = "none";
+          } else {
+            event.target.parentElement.parentElement.style.display = "none";
+          }
+        });
   }
 
-  console.log(decks);
-
-  if (decks) {
+  if (localStorage.getItem("username")) {
     return (
-      <div>
-        <h2>Main page</h2>
-        <p>{`Welcome ${localStorage.getItem("username")}`}</p>
-        <section className="deck-cards">
-          {decks.map((deck) => {
-            return (
-              <div className="deck-card-element">
-                <div className="deck-title">
-                  <p className="deck-text">{deck.name}</p>
-                </div>
-                <div className="deck-description">
-                  <p className="deck-text">{deck.description}</p>
-                </div>
-                <div className="deck-image-container">
-                  <img src={"/deck.png"} className="deck-image" alt="Deck" />
-                </div>
-                <div className="deck-play-button">
-                  <Link to="/game" className="play-link">
-                    <div className="play-button">Study</div>
-                  </Link>
-                </div>
-                <div className="deck-button-container">
-                  <Link
-                    to={`/deck/${deck["id"]}`}
-                    className="decks-cards-button-link"
-                  >
-                    <div className="deck-button-text">Cards</div>
-                  </Link>
-                  <div className="delete-deck-button" onClick={DeleteDeck}>
-                    <div className="deck-button-text">Delete</div>
+        <div>
+          <h2>Main page</h2>
+          <p>{`Welcome ${localStorage.getItem("username")}`}</p>
+          <section className="deck-cards">
+            {decks.map((deck) => {
+              return (
+                  <div key={deck.id} className="deck-card-element">
+                    <div className="deck-title">
+                      <p className="deck-text">{deck.name}</p>
+                    </div>
+                    <div className="deck-description">
+                      <p className="deck-text">{deck.description}</p>
+                    </div>
+                    <div className="deck-image-container">
+                      <img src={"/deck.png"} className="deck-image" alt="Deck"/>
+                    </div>
+                    <div className="deck-play-button">
+                      <Link to="/game" className="play-link">
+                        <div className="play-button">Study</div>
+                      </Link>
+                    </div>
+                    <div className="deck-button-container">
+                      <Link
+                          to={`/deck/${deck["id"]}`}
+                          className="decks-cards-button-link"
+                      >
+                        <div className="deck-button-text">Cards</div>
+                      </Link>
+                      <div className="delete-deck-button" data-id={deck.id} onClick={deleteDeck}>
+                        <div className="deck-button-text" data-id={deck.id}>Delete</div>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            );
-          })}
-        </section>
-      </div>
+              );
+            })}
+          </section>
+        </div>
     );
   } else {
-    return <Login />;
+    return <Login/>;
   }
 };
 
