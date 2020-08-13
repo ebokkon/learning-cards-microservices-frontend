99881453d713eab9d973aa705b79d526a1bd7c71
Kornél <leitner.kornel@gmail.com>
2020-08-13 18:02:17 +0200

    Set game page basic structure and style

--- src/pages/game_page/GamePage.js
+++ src/pages/game_page/GamePage.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./GamePage.css"
 
 const GamePage = () => {
+
+  const [currentCard, setCurrentCard] = useState(
+      {
+        "id": 1,
+        "question": "Budapest",
+        "answer": "Hungary"
+      }
+  );
+
+  console.log(currentCard);
+
   return (
-      <div>
+      <div className="game-page">
         <h2>Game page</h2>
+
+        <div className="game-container">
+          <div className="current-card">
+            <div className="game-picture-container">
+              <img src={"/card_default.png"} alt="Default" className="game-image"/>
+            </div>
+            <div className="game-question">
+              <p>{currentCard.question}</p>
+            </div>
+            <div className="game-answer">
+              <p>{currentCard.answer}</p>
+            </div>
+          </div>
+          <div className="game-button-container">
+            <div className="game-show-answer-button">
+              <div className="game-show-answer-text">Show answer</div>
+            </div>
+            <div className="game-next-card-buttons">
+              <div className="game-positive-button">
+                <div className="game-positive-button-text">Easy</div>
+              </div>
+              <div className="game-negative-button">
+                <div className="game-negative-button-text">Hard</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
   );
 };
